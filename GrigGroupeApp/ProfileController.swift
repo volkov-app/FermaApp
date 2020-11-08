9361f3ff667f745cb6859442911331c5f1f98085
volkov_up@mail.ru <Volkov_alx5>
2020-11-08 19:38:57 +0100

    test2

---
+++ GrigGroupeApp/ProfileController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GrigGroupeApp
+//
+//  Created by user183382 on 10/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
